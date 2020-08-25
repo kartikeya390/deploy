98f122f399db0686eaf7168244ced02e8bb246e5
kartikeya kumar <kartikeyakumar94@gmail.com>
2020-08-25 17:51:46 +0530

    add

--- brigade.js
+++ brigade.js
@@ -1,16 +1,26 @@
 const { events, Job } = require("brigadier");
-events.on("push", () => {
- var job = new Job("buildfinal", "docker:dind");
- job.privileged = true;
- job.tasks = [
-     "dockerd &",
-     "cd /src",
-     "ls -l",
-     "sleep 10",
-     "docker info",
-     "docker ps"
- ];
- 
 
- job.run();
-});
+events.on("exec", function(e, project) {
+  console.log("received push for commit " + e.revision.commit)
+
+  // Create a new job
+   var docker = new Job("job2" , "docker:dind");
+    docker.privileged = true;
+    docker.env = {
+    DOCKER_DRIVER: "overlay"
+    };
+
+  docker.tasks = [
+    "dockerd-entrypoint.sh &",
+    "sleep 10",
+    "ls -lart",
+    "pwd",
+    "docker build -t kartikeya390/dockerdeploy:v4 .",
+    "docker images"
+]
+
+  docker.streamLogs = true;
+
+  // We're done configuring, so we run the job
+  docker.run()
+})
