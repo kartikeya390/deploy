bc14080c2ef0f42fed52925f9bf81eb7e551ea47
kartikeya kumar <kartikeyakumar94@gmail.com>
2020-08-25 13:59:36 +0530

    add

--- html/webapp/Application/brigade.js
+++ html/webapp/Application/brigade.js
@@ -1,31 +1,29 @@
 const { events, Job } = require("brigadier");
 events.on("exec", (e, p) => {
-   console.log("Received push for commit " + e.revision.commit)
-   var commit = e.revision.commit.substr(e.revision.commit.length -7);
-   commit = e.revision.commit.substring(0, 7);
-   var greeting = new Job("job1", "alpine:latest");
-   greeting.storage.enabled = true;
-   greeting.tasks = [
-   "echo Hello Pipeline",
-   `echo commit id is ${commit}`
+    console.log("Received push for commit " + e.revision.commit)
+    var commit = e.revision.commit.substr(e.revision.commit.length - 7);
+    commit = e.revision.commit.substring(0, 7);
+    var greeting = new Job("job1", "alpine:latest");
+    greeting.storage.enabled = true;
+    greeting.tasks = [
+        "echo Hello Pipeline",
+        `echo commit id is ${commit}`
 
-   ]
-   
-    var docker = new Job("job2", "docker:dind");
-    docker.privileged = true;
-    docker.env = {
-    DOCKER_DRIVER: "overlay"
-    }
-    docker.tasks = [
-        "dockerd-entrypoint.sh &",
-        "sleep 10",
-        "pwd",
-        "ls -lrt",
-        "ls -l",
-        "docker build -t kartikeya390/dockerdeploy:test .",
-        "docker images"
-        ];
-
+    ]
+    var docker = new Job("job2" , "docker:dind");
+    docker.privileged = true;
+    docker.env = {
+    DOCKER_DRIVER: "overlay"
+    };
+docker.tasks = [
+    "dockerd-entrypoint.sh &",
+    "sleep 10",
+    "cd /src/html/webapp/Application",
+    "ls -lart",
+    "pwd",
+    "docker build -t brigade .",
+    "docker images",
+]
 
    greeting.run();
    docker.run();
